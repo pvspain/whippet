40b446028e5c812548bd23a8e8b42a29f97c1a03
Paul Spain <pvspain@gmail.com>
2011-12-03 10:24:20 +1100

    new rails app

---
+++ src/whippet/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whippet::Application.config.secret_token = '6682a4a4fdf52d9fd4ad8e1a3077909dbb684536d9a210bf610cc171f64b7ca9e2792e3b47e1efc0f41aa3922d197dc581a87ca0fb752d0b0696d1d21bb430ea'
