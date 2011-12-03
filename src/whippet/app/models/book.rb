cc23dd1e10e2ae248a8c73e63a39b9314192433b
Luke Carter-Key <luke.carterkey@gmail.com>
2011-12-03 13:30:34 +1100

    Added validation to book.

--- src/whippet/app/models/book.rb
+++ src/whippet/app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ActiveRecord::Base
+	validates :title, :isbn, :presence => true
+	validates :isbn, :length => { :is => 10 } 
+	validates :isbn, :numericality => { :only_integer => true }
 end
