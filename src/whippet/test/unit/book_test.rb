4fa3fa942c090a49fab53612ea9ede3bbec71470
Paul Spain <pvspain@gmail.com>
2011-12-04 17:24:48 +1100

    Version tests passing

--- src/whippet/test/unit/book_test.rb
+++ src/whippet/test/unit/book_test.rb
@@ -1,9 +1,11 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
+
   test "should not save a book without title" do
     book = Book.new
     assert book.invalid?
     assert_equal "can't be blank", book.errors[:title].join('; ')
   end
-end
+  
+end 
--- src/whippet/test/unit/version_test.rb
+++ src/whippet/test/unit/version_test.rb
@@ -1,21 +1,38 @@
 require 'test_helper'
 
 class VersionTest < ActiveSupport::TestCase
+  setup do
+    @version = Version.new
+  end
+
   test "Should not save a version with isbn length < 10 chars" do
-    version = Version.new
-    assert(version.invalid?)
-    assert(version.errors[:isbn].any?)
+    assert @version.invalid?
+    assert @version.errors[:isbn].any?
+    assert_equal "is the wrong length (should be 10 characters); is not a number", @version.errors[:isbn].join('; ')
   end
 
   test "Should not save a version with isbn length > 10" do
-  	assert false
+  	assert @version.invalid?
+    @version.isbn = "123456789012"
+    @version.edition = "first"
+    assert @version.errors[:isbn].any?
+    assert_equal "is the wrong length (should be 10 characters); is not a number", @version.errors[:isbn].join('; ')
   end
 
   test "Should save a version with isbn length = 10" do
-  	assert false
+    assert @version.invalid?
+    @version.isbn = "1234567890"
+    @version.edition = "first"
+    assert @version.valid?
+    assert_equal "", @version.errors[:isbn].join('; ')
   end
 
   test "Should not save a version with isbn that contains non numeric characters" do
-  	assert false
+    assert @version.invalid?
+    @version.isbn = "abcdefghij"
+    @version.edition = "first"
+    assert @version.invalid?
+    assert_equal "is not a number", @version.errors[:isbn].join('; ')
   end
+
 end
