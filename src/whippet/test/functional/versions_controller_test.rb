4f998d166c1e93d51542c644fad5a62dc4e65841
Luke Carter-Key <luke.carterkey@gmail.com>
2011-12-04 16:00:29 +1100

    Fixed failing fnctional tests.

--- src/whippet/test/functional/versions_controller_test.rb
+++ src/whippet/test/functional/versions_controller_test.rb
@@ -5,39 +5,19 @@ class VersionsControllerTest < ActionController::TestCase
     @version = versions(:version1)
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
   test "should create version" do
     assert_difference('Version.count') do
-      post :create, :book_id => @version.book_id, version: @version.attributes
+      post :create, version: @version.attributes, :book_id => @version.book_id, version: @version.attributes
     end
 
-    assert_redirected_to version_path(assigns(:version))
-  end
-
-  test "should show version" do
-    get :show, id: @version.to_param
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @version.to_param
-    assert_response :success
-  end
-
-  test "should update version" do
-    put :update, id: @version.to_param, version: @version.attributes
-    assert_redirected_to version_path(assigns(:version))
+    assert_redirected_to @version.book, assigns(:version)
   end
 
   test "should destroy version" do
     assert_difference('Version.count', -1) do
-      delete :destroy, id: @version.to_param
+      delete :destroy, id: @version.to_param, :book_id => @version.book_id
     end
 
-    assert_redirected_to versions_path
+    assert_redirected_to @version.book
   end
 end
--- src/whippet/test/unit/version_test.rb
+++ src/whippet/test/unit/version_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class VersionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Should not save a version with isbn length < 10 chars" do
+    version = Version.new
+    assert(version.invalid?)
+    assert(version.errors[:isbn].any?)
+  end
+
+  test "Should not save a version with isbn length > 10" do
+  	assert false
+  end
+
+  test "Should save a version with isbn length = 10" do
+  	assert false
+  end
+
+  test "Should not save a version with isbn that contains non numeric characters" do
+  	assert false
+  end
 end
