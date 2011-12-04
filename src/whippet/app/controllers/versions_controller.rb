44b2f0bbc555f5198210ef758f6a908866f882bf
Luke Carter-Key <luke.carterkey@gmail.com>
2011-12-04 12:46:57 +1100

    Validation of partial versions view. Still not quite working.

--- src/whippet/app/controllers/versions_controller.rb
+++ src/whippet/app/controllers/versions_controller.rb
@@ -1,8 +1,16 @@
 class VersionsController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
-    @version = @book.versions.create(params[:version])
-    redirect_to book_path(@book)
+    @version = @book.versions.build(params[:version])
+    respond_to do |format|
+      if @version.save
+        format.html { redirect_to @book, notice: 'Version was successfully created.' }
+        format.json { render json: @book, status: :created, location: @book }
+      else
+        format.html { redirect_to book_path @book }
+        format.json { render json: @version.errors, status: :unprocessable_entity }
+      end   
+    end 
   end
   
   def destroy
